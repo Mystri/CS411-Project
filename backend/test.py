6dbed2efd33c11d6a0ecffdc9a983b8e10ede5ca
xuwk <1649464377@qq.com>
2021-10-24 18:14:38 -0500

    get new flask version

--- backend/test.py
+++ backend/test.py
@@ -6,8 +6,9 @@ from flask import Flask,render_template,request,send_from_directory
 import json
 import sqlite3
 import requests
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 conn = connector.connect(
     "festive-planet-281310:us-central1:cs411",
     "pymysql",
@@ -43,4 +44,24 @@ def search_movie(name):
     return {'rec':result}
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=9898, threaded=True, debug=False)
+    app.run(port=8000, debug=True)
+
+# import flask
+# from flask import request  #获取参数
+# from flask_cors import CORS
+
+
+
+# server = flask.Flask(__name__) #创建一个flask对象
+# CORS(server)
+# @server.route('/login', methods=['get','post'])
+# def login():
+#     username = request.values.get('username') #获取参数
+#     password = request.values.get('password')
+#     if username and password:
+#         #sql = 'select User from user where User="%s"'%username
+#         #data = conn_mysql(sql)     
+#             return '{msg:"无数据"}'
+
+
+# server.run(port=8000,debug=True)
