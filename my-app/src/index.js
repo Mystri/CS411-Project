81bd2c4002c82c3d655130e2058330cdeef17cab
xuwk <1649464377@qq.com>
2021-10-24 17:48:46 -0500

    initialize react app

---
+++ my-app/src/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import axios from 'axios';
+
+  class Game extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            banners: ['']
+        }
+    }
+
+    // get请求
+    componentDidMount () {
+        fetch('http://localhost:8000/search_movie/world')
+            .then(data => {
+                console.log('parsed json', data);
+                return data.json()})
+            .then(data => {
+                this.setState({
+                    banners: data.rec
+                });
+                console.log('parsed json', data.rec);            
+            }, (ex) => {
+                this.setState({
+                    requestError : true
+                });
+                console.log('parsing failed', ex)
+            })
+    }   
+
+
+    render(){
+        return(
+            <div>
+                <hr/>
+                <h1>Get </h1>
+                
+               {
+                   this.state.banners.map((photo,index) =>{
+                       return(
+                           <ul key = {index}>
+                               <h3>{index}:{photo}</h3>
+                           </ul>
+                       )
+                   })
+               }
+            </div>
+        )
+    }
+    
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(<Game />, document.getElementById("root"));
+//   ReactDOM.render(<Board />, document.getElementById("root"));
+  
+  
