8272acec55f5b0d9d94ffe52f9900c04d9226049
xuwk <1649464377@qq.com>
2021-10-28 19:06:45 -0500

    add register

--- backend/test.py
+++ backend/test.py
@@ -43,25 +43,24 @@ def search_movie(name):
 
     return {'rec':result}
 
+@app.route("/register", methods=["POST"])
+def register():
+    data = request.get_json(force=True)
+    email = data['email']
+    
+    cursor.execute("SELECT count(*) from user where email='{}'".format(email))
+
+    count = cursor.fetchall()[0][0]
+
+    if count > 0:
+        return {"rec": False}
+    else:
+        cursor.execute("INSERT INTO user VALUES ('{}','{}','{}','','1970-6-22','')".format(data['username'],email,data['password']))
+        conn.commit()
+        return {"rec": True}
+
+
+
+
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
-
-# import flask
-# from flask import request  #获取参数
-# from flask_cors import CORS
-
-
-
-# server = flask.Flask(__name__) #创建一个flask对象
-# CORS(server)
-# @server.route('/login', methods=['get','post'])
-# def login():
-#     username = request.values.get('username') #获取参数
-#     password = request.values.get('password')
-#     if username and password:
-#         #sql = 'select User from user where User="%s"'%username
-#         #data = conn_mysql(sql)     
-#             return '{msg:"无数据"}'
-
-
-# server.run(port=8000,debug=True)
