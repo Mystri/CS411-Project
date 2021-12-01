54b3af5d23ef266cf11ff46d70c2ac6f7de7eb0b
Stranger3333 <1519272170@qq.com>
2021-12-01 00:38:22 -0600

    bold logo

--- my-app/src/List/Mylistdisplay.js
+++ my-app/src/List/Mylistdisplay.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import {
     BrowserRouter as Router,
@@ -8,18 +8,18 @@ import {
     Link
 } from "react-router-dom";
 import { x } from "../LoginComponent/LoginForm_Bootstrap.js";
-import { Card, Container, Stack, ListGroup, Modal } from 'react-bootstrap'
+import { Card, Container, Stack, ListGroup, Modal, Button, Row, Col } from 'react-bootstrap'
 import Getcontent from "./Getcontent.js";
-export default ()=>{
+export default () => {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    const[mylist,setList] = useState([]);
+    const [mylist, setList] = useState([]);
     const email = JSON.parse(window.localStorage.getItem('login')).email;
-    
-        console.log(JSON.parse(window.localStorage.getItem('login')).email)
-        useEffect(() => {
-            const request = {
+
+    console.log(JSON.parse(window.localStorage.getItem('login')).email)
+    useEffect(() => {
+        const request = {
             method: 'POST',
             mode: 'cors',
             credentials: 'omit',
@@ -37,32 +37,35 @@ export default ()=>{
             })
             .catch((error) => {
                 console.log(error)
-              });
-            },[]);
+            });
+    }, []);
 
-const delete_list = () => {
-    
-}
-        
-        return (
-            <div>
-                {/* {this.getmylist()} */}
-                <Card style={{ width: '75%' }}>
-                    <ListGroup>
-                        {mylist.map((myl) => {
-                        return (<Mytab valueProps={myl} />)}
-                        )
-                        }
-                        
-                    </ListGroup>
 
-                </Card>
-            </div>
-        )
+    return (
+        <div>
+            {/* {this.getmylist()} */}
+            <Card style={{ width: '75%' }} className = "d-flex">
+                <ListGroup>
+                    {mylist.map((myl) => {
+                        return (<Mytab valueProps={myl} />)
+                    }
+                    )
+                    }
+
+                </ListGroup>
+
+            </Card>
+        </div>
+    )
 }
 function Mytab(myl) {
     return (
-        <ListGroup.Item id={myl.valueProps.listid} style={{ height: '4em' }}
-                            action href={"/list/"+myl.valueProps.listid}>{myl.valueProps.list_name}</ListGroup.Item>
+        <>
+                <ListGroup.Item id={myl.valueProps.listid} style={{ height: '4em' }}
+                    action href={"/list/" + myl.valueProps.listid}>{myl.valueProps.list_name}</ListGroup.Item>
+
+            
+        </>
+
     )
 }
